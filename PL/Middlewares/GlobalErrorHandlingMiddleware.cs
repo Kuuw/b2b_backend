b7f72a33bbb7e13ad00b6549198cb377f49bae1d
Kuuw <67199397+Kuuw@users.noreply.github.com>
2025-03-20 09:29:34 +0300

    feat: create middleware for global error handling

---
+++ PL/Middlewares/GlobalErrorHandlingMiddleware.cs
@@ -0,0 +1,44 @@
+﻿namespace PL.Middlewares;
+
+using Microsoft.AspNetCore.Http;
+using System.Net;
+using System.Text.Json;
+
+public class GlobalErrorHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public GlobalErrorHandlingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        Console.WriteLine($"Exception: {exception.Message}");
+
+        var response = new
+        {
+            StatusCode = (int)HttpStatusCode.InternalServerError,
+            Message = "An unexpected error occurred. Please try again later.",
+            Detailed = exception.Message
+        };
+
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+        return context.Response.WriteAsync(JsonSerializer.Serialize(response));
+    }
+}
