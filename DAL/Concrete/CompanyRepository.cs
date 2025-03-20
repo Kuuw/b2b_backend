ed3dae0700a6e594070c470394abdd1681b69e86
Kuuw <67199397+Kuuw@users.noreply.github.com>
2025-03-20 10:17:55 +0300

    feat: create GetByEmail function for CompanyRepository

--- DAL/Concrete/CompanyRepository.cs
+++ DAL/Concrete/CompanyRepository.cs
@@ -1,8 +1,23 @@
 ﻿using DAL.Abstract;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DAL.Concrete
 {
     public class CompanyRepository : GenericRepository<Company>, ICompanyRepository
     {
+        private readonly B2bContext _context = new B2bContext();
+        private readonly DbSet<Company> _company;
+
+        public CompanyRepository(B2bContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _company = context.Set<Company>();
+        }
+
+        public Company? GetByEmail(string email)
+        {
+            return _company.FirstOrDefault(c => c.Email == email);
+        }
     }
 }
