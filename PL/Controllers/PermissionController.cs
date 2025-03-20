8dfd001b31d245c0a953738c5009f28fbef6303a
Kuuw <67199397+Kuuw@users.noreply.github.com>
2025-03-20 10:56:17 +0300

    feat: create PermissionController with crud functionalities

---
+++ PL/Controllers/PermissionController.cs
@@ -0,0 +1,43 @@
+﻿using BL.Abstract;
+using Entities.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PL.Controllers
+{
+    public class PermissionController : BaseController
+    {
+        private readonly IPermissionService _permissionService;
+
+        public PermissionController(IPermissionService permissionService)
+        {
+            _permissionService = permissionService;
+        }
+
+        // TODO: Implement permission filters.
+        // TODO: Implement validation filters.
+
+        [HttpGet("{id}")]
+        public IActionResult PermissionGet(Guid id)
+        {
+            return HandleServiceResult(_permissionService.GetById(id));
+        }
+
+        [HttpPost]
+        public IActionResult PermissionPost([FromBody] PermissionPostDto data)
+        {
+            return HandleServiceResult(_permissionService.Insert(data));
+        }
+
+        [HttpPut]
+        public IActionResult PermissionPut([FromBody] PermissionPutDto data)
+        {
+            return HandleServiceResult(_permissionService.Update(data));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult PermissionDelete(Guid id)
+        {
+            return HandleServiceResult(_permissionService.Delete(id));
+        }
+    }
+}
