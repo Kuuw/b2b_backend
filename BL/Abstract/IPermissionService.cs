bd65d08968125e7262c324d378f92aeea9072081
Kuuw <67199397+Kuuw@users.noreply.github.com>
2025-03-20 10:55:08 +0300

    feat: create PermissionService crud fuctionalities

--- BL/Abstract/IPermissionService.cs
+++ BL/Abstract/IPermissionService.cs
@@ -1,6 +1,13 @@
-﻿namespace BL.Abstract
+﻿using Entities.DTO;
+using Entities.Models;
+
+namespace BL.Abstract
 {
     public interface IPermissionService
     {
+        public ServiceResult<bool> Insert(PermissionPostDto data);
+        public ServiceResult<PermissionGetDto?> GetById(Guid id);
+        public ServiceResult<bool> Update(PermissionPutDto data);
+        public ServiceResult<bool> Delete(Guid guid);
     }
 }
--- BL/Concrete/PermissionService.cs
+++ BL/Concrete/PermissionService.cs
@@ -1,8 +1,53 @@
-﻿using BL.Abstract;
+﻿using AutoMapper;
+using BL.Abstract;
+using DAL.Abstract;
+using Entities.DTO;
+using Entities.Models;
 
 namespace BL.Concrete
 {
     public class PermissionService : IPermissionService
     {
+        private readonly IPermissionRepository _permissionRepository;
+        private readonly Mapper mapper = MapperConfig.InitializeAutomapper();
+
+        public PermissionService(IPermissionRepository permissionRepository)
+        {
+            _permissionRepository = permissionRepository;
+        }
+
+        public ServiceResult<bool> Insert(PermissionPostDto data)
+        {
+            var permission = mapper.Map<Permission>(data);
+            var result = _permissionRepository.Insert(permission);
+            return ServiceResult<bool>.Ok(result);
+        }
+
+        public ServiceResult<PermissionGetDto?> GetById(Guid id)
+        {
+            var permission = _permissionRepository.GetById(id);
+            if (permission == null)
+            {
+                return ServiceResult<PermissionGetDto?>.NotFound("Permission not found");
+            }
+            return ServiceResult<PermissionGetDto?>.Ok(mapper.Map<PermissionGetDto?>(permission!));
+        }
+
+        public ServiceResult<bool> Update(PermissionPutDto data)
+        {
+            var permission = mapper.Map<Permission>(data);
+            var result = _permissionRepository.Update(permission);
+            return ServiceResult<bool>.Ok(result);
+        }
+
+        public ServiceResult<bool> Delete(Guid guid)
+        {
+            Permission? permission = _permissionRepository.GetById(guid);
+            if (permission == null)
+            {
+                return ServiceResult<bool>.NotFound("Permission not found");
+            }
+            return ServiceResult<bool>.Ok(_permissionRepository.Delete(permission));
+        }
     }
 }
