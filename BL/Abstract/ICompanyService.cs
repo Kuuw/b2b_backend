71f76e9503d11d1ff098aa2897a87812d21139c1
Kuuw <67199397+Kuuw@users.noreply.github.com>
2025-03-20 10:19:15 +0300

    feat: create crud functions for CompanyService

--- BL/Abstract/ICompanyService.cs
+++ BL/Abstract/ICompanyService.cs
@@ -1,6 +1,14 @@
-﻿namespace BL.Abstract
+﻿using Entities.DTO;
+using Entities.Models;
+
+namespace BL.Abstract
 {
     public interface ICompanyService
     {
+        public ServiceResult<bool> Insert(CompanyPostDto data);
+        public ServiceResult<CompanyGetDto?> GetByEmail(string email);
+        public ServiceResult<CompanyGetDto?> GetById(Guid id);
+        public ServiceResult<bool> Update(CompanyPutDto data);
+        public ServiceResult<bool> Delete(Guid guid);
     }
 }
--- BL/Concrete/CompanyService.cs
+++ BL/Concrete/CompanyService.cs
@@ -1,8 +1,55 @@
-﻿using BL.Abstract;
+﻿using AutoMapper;
+using BL.Abstract;
+using DAL.Abstract;
+using Entities.DTO;
+using Entities.Models;
 
 namespace BL.Concrete
 {
     public class CompanyService : ICompanyService
     {
+        private readonly ICompanyRepository _companyRepository;
+        private readonly Mapper mapper = MapperConfig.InitializeAutomapper();
+
+        public CompanyService(ICompanyRepository companyRepository)
+        {
+            _companyRepository = companyRepository;
+        }
+
+        public ServiceResult<bool> Insert(CompanyPostDto data)
+        {
+            var company = mapper.Map<Company>(data);
+
+            return ServiceResult<bool>.Ok(_companyRepository.Insert(company));
+        }
+
+        public ServiceResult<CompanyGetDto?> GetByEmail(string email)
+        {
+            var company = _companyRepository.GetByEmail(email);
+            return ServiceResult<CompanyGetDto?>.Ok(mapper.Map<CompanyGetDto?>(company));
+        }
+
+        public ServiceResult<CompanyGetDto?> GetById(Guid id)
+        {
+            var company = _companyRepository.GetById(id);
+            return ServiceResult<CompanyGetDto?>.Ok(mapper.Map<CompanyGetDto?>(company));
+        }
+
+        public ServiceResult<bool> Update(CompanyPutDto data)
+        {
+            var company = mapper.Map<Company>(data);
+            return ServiceResult<bool>.Ok(_companyRepository.Update(company));
+        }
+
+        public ServiceResult<bool> Delete(Guid guid)
+        {
+            Company? company = _companyRepository.GetById(guid);
+
+            if (company == null)
+            {
+                return ServiceResult<bool>.NotFound("Company not found");
+            }
+            return ServiceResult<bool>.Ok(_companyRepository.Delete(company));
+        }
     }
 }
