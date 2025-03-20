5a6f68d8dbc96dbc1a73dc347640318c4312c647
Kuuw <67199397+Kuuw@users.noreply.github.com>
2025-03-20 10:22:49 +0300

    feat: update MapperConfig and MapperExtensions to include Company model mappings

--- BL/Concrete/MapperConfig.cs
+++ BL/Concrete/MapperConfig.cs
@@ -1,8 +1,19 @@
-﻿using BL.Abstract;
+﻿using AutoMapper;
+using BL.Abstract;
 
 namespace BL.Concrete
 {
     public class MapperConfig : IMapperConfig
     {
+        public static Mapper InitializeAutomapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddCompanyMappings();
+            });
+
+            var mapper = new Mapper(config);
+            return mapper;
+        }
     }
 }
--- BL/Concrete/MapperExtensions.cs
+++ BL/Concrete/MapperExtensions.cs
@@ -1,6 +1,16 @@
-﻿namespace BL.Concrete
+﻿using AutoMapper;
+using Entities.DTO;
+using Entities.Models;
+
+namespace BL.Concrete
 {
-    public class MapperExtensions
+    public static class MapperExtensions
     {
+        public static void AddCompanyMappings(this IMapperConfigurationExpression cfg)
+        {
+            cfg.CreateMap<Company, CompanyGetDto>();
+            cfg.CreateMap<CompanyPostDto, Company>();
+            cfg.CreateMap<CompanyPutDto, Company>();
+        }
     }
 }
