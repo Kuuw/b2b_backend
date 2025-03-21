cee85320a0a85c2d6b1602227237468be1b1ab3a
Kuuw <67199397+Kuuw@users.noreply.github.com>
2025-03-21 08:40:27 +0300

    feat: update LogService interface

--- BL/Abstract/ILogService.cs
+++ BL/Abstract/ILogService.cs
@@ -1,6 +1,26 @@
-﻿namespace BL.Abstract
+﻿using Entities.DTO;
+using Entities.Models;
+
+namespace BL.Abstract
 {
     public interface ILogService
     {
+        public ServiceResult<bool> Insert(LogPostDto data);
+
+        public ServiceResult<LogGetDto?> GetById(Guid id);
+
+        public ServiceResult<List<LogGetDto>> GetPaged(int page, int pageSize);
+
+        public ServiceResult<bool> Delete(Guid id);
+
+        public ServiceResult<bool> InsertType(LogTypePostDto data);
+
+        public ServiceResult<LogTypeGetDto?> GetTypeById(Guid id);
+
+        public ServiceResult<List<LogTypeGetDto>> GetTypesPaged(int page, int pageSize);
+
+        public ServiceResult<bool> UpdateType(LogTypePutDto data);
+
+        public ServiceResult<bool> DeleteType(Guid id);
     }
 }
