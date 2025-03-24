2981843ff78051fa3d591aaf3e75a749a01e70ee
Kuuw <67199397+Kuuw@users.noreply.github.com>
2025-03-24 22:25:41 +0300

    feat: create GenericService

---
+++ BL/Abstract/IGenericService.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Models;
+
+namespace BL.Abstract
+{
+    public interface IGenericService<TModel, TPostDto, TGetDto, TPutDto>
+    {
+        ServiceResult<bool> Insert(TPostDto data);
+        ServiceResult<TGetDto?> GetById(Guid id);
+        ServiceResult<List<TGetDto>> GetPaged(int page, int pageSize);
+        ServiceResult<bool> Delete(Guid id);
+        ServiceResult<bool> Update(TPutDto data);
+    }
+}
---
+++ BL/Concrete/GenericService.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using BL.Abstract;
+using DAL.Abstract;
+using Entities.Models;
+
+namespace BL.Concrete
+{
+    public class GenericService<TModel, TPostDto, TGetDto, TPutDto> : IGenericService<TModel, TPostDto, TGetDto, TPutDto>
+        where TModel : class
+    {
+        private readonly IGenericRepository<TModel> _repository;
+        private readonly Mapper _mapper = MapperConfig.InitializeAutomapper();
+
+        public GenericService(IGenericRepository<TModel> repository)
+        {
+            _repository = repository;
+        }
+
+        public ServiceResult<bool> Insert(TPostDto data)
+        {
+            var model = _mapper.Map<TModel>(data);
+            return ServiceResult<bool>.Ok(_repository.Insert(model));
+        }
+
+        public ServiceResult<TGetDto?> GetById(Guid id)
+        {
+            var model = _repository.GetById(id);
+            return ServiceResult<TGetDto?>.Ok(_mapper.Map<TGetDto?>(model));
+        }
+
+        public ServiceResult<List<TGetDto>> GetPaged(int page, int pageSize)
+        {
+            var models = _repository.GetPaged(page, pageSize);
+            return ServiceResult<List<TGetDto>>.Ok(_mapper.Map<List<TGetDto>>(models));
+        }
+
+        public ServiceResult<bool> Delete(Guid id)
+        {
+            var model = _repository.GetById(id);
+            if (model == null)
+            {
+                return ServiceResult<bool>.NotFound($"{typeof(TModel).Name} not found");
+            }
+            return ServiceResult<bool>.Ok(_repository.Delete(model));
+        }
+
+        public ServiceResult<bool> Update(TPutDto data)
+        {
+            var model = _mapper.Map<TModel>(data);
+            return ServiceResult<bool>.Ok(_repository.Update(model));
+        }
+    }
+}
