6c8aeb6cd52bf379383d83a8c388052212e61455
Kuuw <67199397+Kuuw@users.noreply.github.com>
2025-03-26 20:58:04 +0300

    feat: create register function for UserService

--- BL/Abstract/IUserService.cs
+++ BL/Abstract/IUserService.cs
@@ -5,5 +5,6 @@ namespace BL.Abstract
 {
     public interface IUserService : IGenericService<User, UserPostDto, UserGetDto, UserPutDto>
     {
+        ServiceResult<bool> Register(UserRegister userRegister);
     }
 }
--- BL/Concrete/UserService.cs
+++ BL/Concrete/UserService.cs
@@ -1,4 +1,5 @@
-﻿using BL.Abstract;
+﻿using AutoMapper;
+using BL.Abstract;
 using DAL.Abstract;
 using Entities.DTO;
 using Entities.Models;
@@ -8,10 +9,27 @@ namespace BL.Concrete
     public class UserService : GenericService<User, UserPostDto, UserGetDto, UserPutDto>, IUserService
     {
         private readonly IUserRepository _userRepository;
+        private readonly IBcryptService _bcryptService;
+        private readonly Mapper _mapper = MapperConfig.InitializeAutomapper();
 
-        public UserService(IUserRepository userRepository) : base(userRepository)
+        public UserService(IUserRepository userRepository, IBcryptService bcryptService) : base(userRepository)
         {
             _userRepository = userRepository;
+            _bcryptService = bcryptService;
+        }
+
+        public ServiceResult<bool> Register(UserRegister userRegister)
+        {
+            var user = _mapper.Map<User>(userRegister);
+            user.PasswordHash = _bcryptService.HashPassword(userRegister.Password);
+
+            var userData = _userRepository.Insert(user);
+
+            if (userData == null)
+            {
+                return ServiceResult<bool>.InternalServerError("User could not be registered.");
+            }
+            return ServiceResult<bool>.Ok(true);
         }
     }
 }
