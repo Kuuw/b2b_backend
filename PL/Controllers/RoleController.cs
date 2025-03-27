44805c825808d96c68b961dbb7be3edc3d6f53a0
Kuuw <67199397+Kuuw@users.noreply.github.com>
2025-03-27 10:45:08 +0300

    feat: create RoleController

---
+++ PL/Controllers/RoleController.cs
@@ -0,0 +1,53 @@
+﻿using BL.Abstract;
+using Entities.DTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PL.ActionFilters;
+
+namespace PL.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [ApiVersion("1.0")]
+    public class RoleController : BaseController
+    {
+        private readonly IRoleService _roleService;
+
+        public RoleController(IRoleService roleService)
+        {
+            _roleService = roleService;
+        }
+
+        [HttpGet("{id}")]
+        [Authorize]
+        [NeedsPermission("ViewRoleDetails")]
+        public IActionResult RoleGet(Guid id)
+        {
+            return HandleServiceResult(_roleService.GetById(id));
+        }
+
+        [HttpPost]
+        [Authorize]
+        [NeedsPermission("InsertRole")]
+        public IActionResult RolePost([FromBody] RolePostDto data)
+        {
+            return HandleServiceResult(_roleService.Insert(data));
+        }
+
+        [HttpPut]
+        [Authorize]
+        [NeedsPermission("UpdateRole")]
+        public IActionResult RolePut([FromBody] RolePutDto data)
+        {
+            return HandleServiceResult(_roleService.Update(data));
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize]
+        [NeedsPermission("DeleteRole")]
+        public IActionResult RoleDelete(Guid id)
+        {
+            return HandleServiceResult(_roleService.Delete(id));
+        }
+    }
+}
