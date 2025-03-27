e60614354bcd2cb3a3aee9cbce9f55205b2f4791
Kuuw <67199397+Kuuw@users.noreply.github.com>
2025-03-27 10:44:45 +0300

    feat: create OrderController

---
+++ PL/Controllers/OrderController.cs
@@ -0,0 +1,53 @@
+﻿using BL.Abstract;
+using Entities.DTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PL.ActionFilters;
+
+namespace PL.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [ApiVersion("1.0")]
+    public class OrderController : BaseController
+    {
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [HttpGet("{id}")]
+        [Authorize]
+        [NeedsPermission("ViewOrderDetails")]
+        public IActionResult OrderGet(Guid id)
+        {
+            return HandleServiceResult(_orderService.GetById(id));
+        }
+
+        [HttpPost]
+        [Authorize]
+        [NeedsPermission("CreateOrder")]
+        public IActionResult OrderPost([FromBody] OrderPostDto data)
+        {
+            return HandleServiceResult(_orderService.Insert(data));
+        }
+
+        [HttpPut]
+        [Authorize]
+        [NeedsPermission("UpdateOrder")]
+        public IActionResult OrderPut([FromBody] OrderPutDto data)
+        {
+            return HandleServiceResult(_orderService.Update(data));
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize]
+        [NeedsPermission("DeleteOrder")]
+        public IActionResult OrderDelete(Guid id)
+        {
+            return HandleServiceResult(_orderService.Delete(id));
+        }
+    }
+}
