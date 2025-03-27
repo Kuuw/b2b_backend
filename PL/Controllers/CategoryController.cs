0e834597cf164b02e71a9766cdc69fe65c77daae
Kuuw <67199397+Kuuw@users.noreply.github.com>
2025-03-27 10:44:27 +0300

    feat: create CategoryController

---
+++ PL/Controllers/CategoryController.cs
@@ -0,0 +1,52 @@
+﻿using BL.Abstract;
+using Entities.DTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PL.ActionFilters;
+
+namespace PL.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [ApiVersion("1.0")]
+    public class CategoryController : BaseController
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet("{id}")]
+        [AllowAnonymous]
+        public IActionResult CategoryGet(Guid id)
+        {
+            return HandleServiceResult(_categoryService.GetById(id));
+        }
+
+        [HttpPost]
+        [Authorize]
+        [NeedsPermission("InsertCategory")]
+        public IActionResult CategoryPost([FromBody] CategoryPostDto data)
+        {
+            return HandleServiceResult(_categoryService.Insert(data));
+        }
+
+        [HttpPut]
+        [Authorize]
+        [NeedsPermission("UpdateCategory")]
+        public IActionResult CategoryPut([FromBody] CategoryPutDto data)
+        {
+            return HandleServiceResult(_categoryService.Update(data));
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize]
+        [NeedsPermission("DeleteCategory")]
+        public IActionResult CategoryDelete(Guid id)
+        {
+            return HandleServiceResult(_categoryService.Delete(id));
+        }
+    }
+}
