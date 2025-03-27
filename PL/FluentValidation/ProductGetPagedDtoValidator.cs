f4e6503e8ab6597d1d2259a9531f7a90f906664e
Kuuw <67199397+Kuuw@users.noreply.github.com>
2025-03-27 13:12:06 +0300

    feat: create a validator for ProductGetPagedDto

---
+++ PL/FluentValidation/ProductGetPagedDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using Entities.DTO;
+using FluentValidation;
+
+namespace PL.FluentValidation
+{
+    public class ProductGetPagedDtoValidator : AbstractValidator<ProductGetPagedDto>
+    {
+        public ProductGetPagedDtoValidator()
+        {
+            RuleFor(model => model.Page).GreaterThan(0).WithMessage("Sayfa numarası 0'dan büyük olmalıdır.");
+            RuleFor(model => model.PageSize).GreaterThan(0).WithMessage("Sayfa boyutu 0'dan büyük olmalıdır.");
+            RuleFor(model => model.PageSize).LessThan(30).WithErrorCode("PageSizeLessThan30").WithMessage("Sayfa boyutu 30'dan küçük olmalıdır.");
+        }
+    }
+}
