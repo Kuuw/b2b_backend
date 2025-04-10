a1e270e24a0268327de019a73ff67462ae8a2287
Kuuw <67199397+Kuuw@users.noreply.github.com>
2025-04-10 10:36:45 +0300

    feat: create UploadImage method for ProductService

--- BL/Abstract/IProductService.cs
+++ BL/Abstract/IProductService.cs
@@ -1,10 +1,12 @@
 ﻿using Entities.DTO;
 using Entities.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace BL.Abstract
 {
     public interface IProductService : IGenericService<Product, ProductPostDto, ProductGetDto, ProductPutDto>
     {
         ServiceResult<ProductPagedResponse> GetPaged(ProductGetPagedDto productGetPagedDto);
+        ServiceResult<string> UploadImage(Guid productId, Stream file);
     }
 }
--- BL/Concrete/ProductService.cs
+++ BL/Concrete/ProductService.cs
@@ -3,17 +3,20 @@ using BL.Abstract;
 using DAL.Abstract;
 using Entities.DTO;
 using Entities.Models;
+using ImageMagick;
 
 namespace BL.Concrete
 {
     public class ProductService : GenericService<Product, ProductPostDto, ProductGetDto, ProductPutDto>, IProductService
     {
         private readonly IProductRepository _productRepository;
+        private readonly IFileRepository _fileRepository;
         private readonly Mapper mapper = MapperConfig.InitializeAutomapper();
 
-        public ProductService(IProductRepository productRepository) : base(productRepository)
+        public ProductService(IProductRepository productRepository, IFileRepository fileRepository) : base(productRepository)
         {
             _productRepository = productRepository;
+            _fileRepository = fileRepository;
         }
 
         public ServiceResult<ProductPagedResponse> GetPaged(ProductGetPagedDto productGetPagedDto)
@@ -34,6 +37,63 @@ namespace BL.Concrete
             response.Metadata = metadata;
 
             return ServiceResult<ProductPagedResponse>.Ok(response);
+        }
+
+        public ServiceResult<string> UploadImage(Guid productId, Stream file)
+        {
+            var product = _productRepository.GetById(productId);
+            if (product == null)
+            {
+                throw new ArgumentException("Product not found.");
+            }
+
+            var newGuid = Guid.NewGuid();
+            ProductImage productImage = new();
+            productImage.ProductId = productId;
+            productImage.ProductImageId = newGuid;
+
+
+            using (var seekableStream = new MemoryStream())
+            {
+                file.CopyToAsync(seekableStream);
+                seekableStream.Position = 0;
+                using (var img = new MagickImage(seekableStream))
+                {
+                    if (!(img.Format == MagickFormat.Jpeg || img.Format == MagickFormat.Png))
+                        throw new InvalidOperationException("Uploaded file should be in png or jpeg.");
+
+                    Console.WriteLine($"Image format detected: {img.Format}");
+
+                    var tempFile = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+
+                    try
+                    {
+                        img.Resize(new MagickGeometry(720, 720) { IgnoreAspectRatio = false });
+                        img.Write(tempFile, MagickFormat.Jpeg);
+
+                        using (var tempFileStream = new FileStream(tempFile, FileMode.Open))
+                        {
+                            var path = _fileRepository.UploadFile("product-image", $"{newGuid}.jpg", tempFileStream);
+                            productImage.ImageUrl = path;
+
+                            _productRepository.AddImage(productImage);
+                            return ServiceResult<string>.Ok(path);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        throw;
+                    }
+                    finally
+                    {
+                        if (File.Exists(tempFile))
+                        {
+                            File.Delete(tempFile);
+                        }
+                    }
+                }
+            }
         }
     }
 }
