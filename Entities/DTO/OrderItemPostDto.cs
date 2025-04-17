086d0e7d18959a3064efec7e6ce4e171fdb64568
Kuuw <67199397+Kuuw@users.noreply.github.com>
2025-04-17 11:12:48 +0300

    fix: update order dtos

--- Entities/DTO/OrderItemPostDto.cs
+++ Entities/DTO/OrderItemPostDto.cs
@@ -2,13 +2,7 @@
 
 public partial class OrderItemPostDto
 {
-    public Guid OrderId { get; set; }
-
     public Guid ProductId { get; set; }
 
     public int Quantity { get; set; }
-
-    public double Price { get; set; }
-
-    public string Currency { get; set; } = null!;
 }
--- Entities/DTO/OrderPostDto.cs
+++ Entities/DTO/OrderPostDto.cs
@@ -2,12 +2,6 @@
 
 public partial class OrderPostDto
 {
-    public Guid OrderId { get; set; }
-
-    public Guid UserId { get; set; }
-
-    public Guid StatusId { get; set; }
-
     public Guid ShippingAddressId { get; set; }
 
     public Guid InvoiceAddressId { get; set; }
