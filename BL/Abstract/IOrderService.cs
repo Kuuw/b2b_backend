1a8c4a38919c19fd952884485403286f06b590e8
Kuuw <67199397+Kuuw@users.noreply.github.com>
2025-04-17 11:11:55 +0300

    feat: create SelfCreate and SelfGet for OrderService

--- BL/Abstract/IOrderService.cs
+++ BL/Abstract/IOrderService.cs
@@ -5,5 +5,7 @@ namespace BL.Abstract
 {
     public interface IOrderService : IGenericService<Order, OrderPostDto, OrderGetDto, OrderPutDto>
     {
+        public ServiceResult<bool> SelfCreate(OrderPostDto orderDto);
+        public ServiceResult<List<OrderGetDto>?> SelfGet();
     }
 }
--- BL/Concrete/OrderService.cs
+++ BL/Concrete/OrderService.cs
@@ -1,5 +1,7 @@
-﻿using BL.Abstract;
+﻿using AutoMapper;
+using BL.Abstract;
 using DAL.Abstract;
+using Entities.Context.Abstract;
 using Entities.DTO;
 using Entities.Models;
 
@@ -8,10 +10,38 @@ namespace BL.Concrete
     public class OrderService : GenericService<Order, OrderPostDto, OrderGetDto, OrderPutDto>, IOrderService
     {
         private readonly IOrderRepository _orderRepository;
+        private readonly Mapper _mapper = MapperConfig.InitializeAutomapper();
+        private readonly IUserContext _userContext;
 
-        public OrderService(IOrderRepository orderRepository) : base(orderRepository)
+        public OrderService(IOrderRepository orderRepository, IUserContext userContext) : base(orderRepository)
         {
             _orderRepository = orderRepository;
+            _userContext = userContext;
+        }
+
+        public ServiceResult<bool> SelfCreate(OrderPostDto orderDto) {
+            var order = _mapper.Map<Order>(orderDto);
+            Console.WriteLine(order.OrderItems.Count);
+
+            order.UserId = _userContext.UserId;
+
+            var orderData = _orderRepository.Insert(order);
+            if (orderData == null)
+            {
+                return ServiceResult<bool>.InternalServerError("Order could not be created.");
+            }
+
+            return ServiceResult<bool>.Ok(orderData);
+        }
+
+        public ServiceResult<List<OrderGetDto>?> SelfGet()
+        {
+            var orders = _orderRepository.Where(x => x.UserId == _userContext.UserId);
+            if (orders == null)
+            {
+                return ServiceResult<List<OrderGetDto>?>.NotFound("Orders not found.");
+            }
+            return ServiceResult<List<OrderGetDto>?>.Ok(_mapper.Map<List<OrderGetDto>>(orders));
         }
     }
 }
