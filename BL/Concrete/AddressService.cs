05146df54c2d0336f92aec4376cf8db811664254
Kuuw <67199397+Kuuw@users.noreply.github.com>
2025-04-17 11:12:16 +0300

    feat: create GetSelfAddresses for AddressService

--- BL/Concrete/AddressService.cs
+++ BL/Concrete/AddressService.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using BL.Abstract;
 using DAL.Abstract;
+using Entities.Context.Abstract;
 using Entities.DTO;
 using Entities.Models;
 
@@ -10,10 +11,12 @@ namespace BL.Concrete
     {
         private readonly IAddressRepository _addressRepository;
         private readonly Mapper _mapper = MapperConfig.InitializeAutomapper();
+        private readonly IUserContext _userContext;
 
-        public AddressService(IAddressRepository addressRepository) : base(addressRepository)
+        public AddressService(IAddressRepository addressRepository, IUserContext userContext) : base(addressRepository)
         {
             _addressRepository = addressRepository;
+            _userContext = userContext;
         }
 
         public ServiceResult<List<AddressGetDto>?> GetByUserId(Guid userId)
@@ -25,6 +28,12 @@ namespace BL.Concrete
         public ServiceResult<List<AddressGetDto>?> GetByCompanyId(Guid companyId)
         {
             var address = _addressRepository.Where(x => x.CompanyId == companyId);
+            return ServiceResult<List<AddressGetDto>?>.Ok(_mapper.Map<List<AddressGetDto>?>(address));
+        }
+
+        public ServiceResult<List<AddressGetDto>?> GetSelfAddresses()
+        {
+            var address = _addressRepository.Where(x => x.UserId == _userContext.UserId);
             return ServiceResult<List<AddressGetDto>?>.Ok(_mapper.Map<List<AddressGetDto>?>(address));
         }
     }
