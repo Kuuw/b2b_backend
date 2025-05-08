4da18ac9dadb2a595a6bb8dfa384e193ad5bf5ea
Kuuw <67199397+Kuuw@users.noreply.github.com>
2025-05-08 09:34:02 +0300

    feat: create new company repository methods for report creation

--- DAL/Abstract/ICompanyRepository.cs
+++ DAL/Abstract/ICompanyRepository.cs
@@ -5,5 +5,9 @@ namespace DAL.Abstract
     public interface ICompanyRepository : IGenericRepository<Company>
     {
         public Company? GetByEmail(string email);
+        public int getUserCount(Guid companyId);
+        public float getTotalSales(Guid companyId);
+        public int getTotalOrders(Guid companyId);
+        public Double getAverageSpent(Guid companyId);
     }
 }
--- DAL/Concrete/CompanyRepository.cs
+++ DAL/Concrete/CompanyRepository.cs
@@ -19,5 +19,34 @@ namespace DAL.Concrete
         {
             return _company.FirstOrDefault(c => c.Email == email);
         }
-    }
+
+        public int getUserCount(Guid companyId)
+        {
+            return _context.Users.Count(u => u.CompanyId == companyId);
+        }
+
+        public float getTotalSales(Guid companyId)
+        {
+            var orders = _context.Orders.Where(i => i.User.CompanyId == companyId);
+            var totalSales = orders
+                .SelectMany(o => o.OrderItems)
+                .Sum(oi => (float)(oi.Quantity * oi.Product.Price));
+            return totalSales;
+        }
+
+        public int getTotalOrders(Guid companyId)
+        {
+            return _context.Orders.Count(o => o.User.CompanyId == companyId);
+        }
+
+        public Double getAverageSpent(Guid companyId)
+        {
+            var orders = _context.Orders.Where(i => i.User.CompanyId == companyId);
+            var totalSpent = orders
+                .SelectMany(o => o.OrderItems)
+                .Sum(oi => (float)(oi.Quantity * oi.Product.Price));
+            var orderCount = orders.Count();
+            return orderCount > 0 ? totalSpent / orderCount : 0;
+        }
+}
 }
