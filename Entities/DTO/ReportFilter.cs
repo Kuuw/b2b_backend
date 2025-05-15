904dfefa8999b96540e30b72fbdfb6c94cb054a5
Kuuw <67199397+Kuuw@users.noreply.github.com>
2025-05-15 10:40:37 +0300

    feat: create report paged dtos

---
+++ Entities/DTO/ReportFilter.cs
@@ -0,0 +1,13 @@
+﻿namespace Entities.DTO;
+
+public class ReportFilter
+{
+    public string? Search { get; set; }
+    public List<Guid> Users { get; set; } = [];
+    public DateTime? StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+    public Double? MinSpent { get; set; }
+    public Double? MaxSpent { get; set; }
+    public Double? MinOrder { get; set; }
+    public Double? MaxOrder { get; set; }
+}
---
+++ Entities/DTO/ReportPagedFilter.cs
@@ -0,0 +1,5 @@
+﻿namespace Entities.DTO;
+
+public class ReportPagedFilter: GenericPagedFilter<ReportFilter>
+{
+}
