60f52440c241b5d7894dcb323f590a2a54021b7f
Kuuw <67199397+Kuuw@users.noreply.github.com>
2025-05-15 10:40:10 +0300

    feat: create generic paged filter and response for paged functions

---
+++ Entities/DTO/GenericPagedFilter.cs
@@ -0,0 +1,8 @@
+﻿namespace Entities.DTO;
+
+public partial class GenericPagedFilter<T> where T : class
+{
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
+    public T Filter { get; set; } = default!;
+}
---
+++ Entities/DTO/GenericPagedResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.DTO;
+
+public partial class GenericPagedResponse<T> where T : class
+{
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
+    public int TotalPages { get; set; }
+    public IEnumerable<T> Items { get; set; } = new List<T>();
+}
