389b3f341758bb38d3758c91f4bd27e7946c48c6
Kuuw <67199397+Kuuw@users.noreply.github.com>
2025-05-15 10:48:28 +0300

    feat: add filters to GetReports company method

--- BL/Concrete/CompanyService.cs
+++ BL/Concrete/CompanyService.cs
@@ -4,6 +4,7 @@ using DAL.Abstract;
 using Entities.DTO;
 using Entities.Models;
 using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace BL.Concrete
 {
@@ -30,20 +31,62 @@ namespace BL.Concrete
             return ServiceResult<List<CompanyGetDto>>.Ok(mapper.Map<List<CompanyGetDto>>(companies));
         }
 
-        public ServiceResult<List<CompanyReportDto>> GetReports(int page, int pageSize)
+        public ServiceResult<ReportPagedResponse> GetReports(ReportPagedFilter filter)
         {
-            var companies = _companyRepository.GetPaged(page, pageSize, q => q.Include(x => x.Status).Include(x => x.Address));
-            var companyReports = companies.Select(company => new CompanyReportDto
+            IQueryable<Company> query = _companyRepository.Queryable()
+                .Include(x => x.Status)
+                .Include(x => x.Address)
+                .Where(x => x.CompanyName.Contains(filter.Filter.Search ?? ""))
+                .Where(x => filter.Filter.StartDate == null
+                    || x.Users.SelectMany(u => u.Orders).Any(o => o.CreatedAt >= filter.Filter.StartDate))
+                .Where(x => filter.Filter.EndDate == null
+                    || x.Users.SelectMany(u => u.Orders).Any(o => o.CreatedAt <= filter.Filter.EndDate))
+                .Where(x => filter.Filter.MinSpent == null
+                    || x.Users.SelectMany(u => u.Orders)
+                        .SelectMany(o => o.OrderItems)
+                        .Sum(oi => oi.Product.Price * oi.Quantity) >= filter.Filter.MinSpent)
+                .Where(x => filter.Filter.MaxSpent == null
+                    || x.Users.SelectMany(u => u.Orders)
+                        .SelectMany(o => o.OrderItems)
+                        .Sum(oi => oi.Product.Price * oi.Quantity) <= filter.Filter.MaxSpent)
+                .Where(x => filter.Filter.MinOrder == null
+                    || x.Users.SelectMany(u => u.Orders).Count() >= filter.Filter.MinOrder)
+                .Where(x => filter.Filter.MaxOrder == null
+                    || x.Users.SelectMany(u => u.Orders).Count() >= filter.Filter.MaxOrder);
+
+            if (filter.Filter.Users.Count != 0) { query = query.Where(x => x.Users.Any(u => filter.Filter.Users.Contains(u.UserId))); }
+
+            var companies = _companyRepository.GetPaged(
+                    filter.PageNumber,
+                    filter.PageSize,
+                    q => query
+            );
+
+            var companyReports = new List<CompanyReportDto>();
+            foreach (var company in companies)
             {
-                CompanyId = company.CompanyId,
-                CompanyName = company.CompanyName,
-                UserCount = _companyRepository.getUserCount(company.CompanyId),
-                AverageSpent = _companyRepository.getAverageSpent(company.CompanyId),
-                TotalSpent = _companyRepository.getTotalSales(company.CompanyId),
-                TotalOrders = _companyRepository.getTotalOrders(company.CompanyId),
-                LastOrderDate = _companyRepository.lastOrderDate(company.CompanyId),
-            }).ToList();
-            return ServiceResult<List<CompanyReportDto>>.Ok(companyReports);
+                var report = new CompanyReportDto
+                {
+                    CompanyId = company.CompanyId,
+                    CompanyName = company.CompanyName,
+                    UserCount = _companyRepository.getUserCount(company.CompanyId, filter.Filter),
+                    TotalSpent = _companyRepository.getTotalSales(company.CompanyId, filter.Filter),
+                    TotalOrders = _companyRepository.getTotalOrders(company.CompanyId, filter.Filter),
+                    AverageSpent = _companyRepository.getAverageSpent(company.CompanyId, filter.Filter),
+                    LastOrderDate = _companyRepository.lastOrderDate(company.CompanyId, filter.Filter)
+                };
+                companyReports.Add(report);
+            }
+
+            var response = new ReportPagedResponse
+            {
+                TotalPages = _companyRepository.GetPageCount(filter.PageSize, q => query),
+                PageNumber = filter.PageNumber,
+                PageSize = filter.PageSize,
+                Items = companyReports
+            };
+
+            return ServiceResult<ReportPagedResponse>.Ok(response);
         }
     }
 }
