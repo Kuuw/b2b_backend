741f7654eeedcf040a360d060d40b9f3f9d2c931
Kuuw <67199397+Kuuw@users.noreply.github.com>
2025-05-22 10:21:57 +0300

    feat: update product filter to add product stock filters

--- Entities/DTO/ProductFilter.cs
+++ Entities/DTO/ProductFilter.cs
@@ -6,11 +6,15 @@ public partial class ProductFilter
     public Guid? CategoryId { get; set; }
     public double? MinPrice { get; set; }
     public double? MaxPrice { get; set; }
+    public int? MinStock { get; set; }
+    public int? MaxStock { get; set; }
 
     public ProductFilter()
     {
         CategoryId = null;
         MinPrice = null;
         MaxPrice = null;
+        MinStock = null;
+        MaxStock = null;
     }
 }
